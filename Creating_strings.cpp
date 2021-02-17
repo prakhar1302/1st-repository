55706d222a1606d7b5d56c03637a269327edd703
Prakhar Sharma <77616550+prakhar1302@users.noreply.github.com>
2021-02-17 22:47:18 +0530

    Add files via upload

---
+++ Creating_strings.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+string s;
+cin>>s;
+sort(s.begin(),s.end());
+vector <string> v;
+do
+{
+    v.push_back(s);
+}while(next_permutation(s.begin(),s.end()));
+cout<<v.size()<<endl;
+for(string k : v)
+    cout<<k<<endl;
+return 0;
+}
+
